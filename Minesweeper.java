38027a0f97b55a1e609ef9e1180c288b1d9242fd
subah mehrotra <subahmehrotra@subahs-MacBook-Air.local>
2017-01-30 21:33:44 -0500

    Fix line 55 and 36

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,31 +1,88 @@
+
+// FIX LINE 55 AND 36
+
+
 import java.util.Scanner;
 //import static java.util.Arrays.*;
 
 public class Minesweeper{
 	
-	
-	//function to check if any nearby block has a bomb
-	public boolean nearbyBombs(int r, int c, int row, int col, char arr){
-		 int i, j;
-		 boolean rCount = false;
-		 if(r != 0 || r != row-1 || c != 1 || c != col){
-		 	if(arr[r][c+1]=='X' || arr[r][c-1]=='X' || arr[r+1][c]=='X' || arr[r-1][c]=='X'){
-		 		rCount = true;
-
-	//function return the number of nearby blocks having bombs
+	//Global Variables
+	int row = 0;
+	int col = 0; 
+	int boom = 0;
+	boolean win = false;
+	boolean loose = false;
 	
 	
-	public static void main(String[] args){
-		
-		//Asking for user input 
+	//A function asking user to input the details of the screen
+	public void userInput(){
 		Scanner input = new Scanner(System.in);
-		int row, col, boom;
 		System.out.println("Enter the number of rows");
 		row = input.nextInt();
 		System.out.println("Enter the number of columns");
 		col = input.nextInt();
 		System.out.println("Set the number of bombs");
 		boom = input.nextInt();
+	}
+	
+	//A function to place the bombs randomly
+	public void placeBombs(){
+		char[][] bombs = new char[row][col+1];
+		int i;						          
+		for (i = 0; i < boom; i++){ 						    	//Loop to keep a count of bombs placed
+            int ROW = (int)(Math.random() * ((row-1) - 0) + 0); 	//Getting a random row
+            int COL = (int)(Math.random() * (col - 1) + 1); 		//Getting a random column
+            if(bombs[ROW][COL] == null){ 							//Checking if the cells is empty
+            	bombs[ROW][COL] = 'X'; 								//Then place a bomb
+            }
+            else{
+            	i--; 												//Else look for another cell, without increasing the number of bombs placed
+            }
+        }
+    }
+	
+	//A function to ask the user to select a cell
+	public String cellInput(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Select a cell by entering row and column");
+		int selectedRow = 0; 
+		int selectedCol = 0;
+		selectedRow = input.nextInt();
+		selectedCol = input.nextInt();
+		char a = (char)selectedRow;
+		char b = (char)selectedCol;
+		return (a+b);
+	}
+	
+	
+	//A function to print the cells 
+	public void printCells(char screen[][]){
+		int k,l;
+		for(k=0; k<row; k++){
+			for(l=0; l<col+1; l++){
+				System.out.print(screen[k][l]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	//function to check if any nearby block has a bomb
+	/*public boolean nearbyBombs(int r, int c, int row, int col, char arr){
+		 int i, j;
+		 boolean rCount = false;
+		 if(r != 0 || r != row-1 || c != 1 || c != col){
+		 	if(arr[r][c+1]=='X' || arr[r][c-1]=='X' || arr[r+1][c]=='X' || arr[r-1][c]=='X'){
+		 		rCount = true;*/
+
+	//function return the number of nearby blocks having bombs
+	
+	
+	public void main(String[] args){
+		
+		//Asking for user input 
+		userInput();
 		
 		char[][] screen = new char[row][col+1];
 		
@@ -51,23 +108,11 @@ public class Minesweeper{
 			}
 		}
 		
-		//Printing the screen array
-		int k,l;
-		for(k=0; k<row; k++){
-			for(l=0; l<col+1; l++){
-				System.out.print(screen[k][l]+" ");
-			}
-			System.out.println();
-		}
+		printCells(screen);
+		placeBombs();
+
 		
-		System.out.println((int)(Math.random() * (5 - 0)) + 0);// for row   1 for col
-		
-		/*char[][] bombs = new char[row][col+1];
-		for (int ii = 0; ii < row; ii++){
-            for (int jj = 1; jj < col+1; jj++){
-                bombs[ii][jj] = ((int) Math.random() * 5 + 3);
-            }
-        }*/
+
         
 		
 	}//end of main

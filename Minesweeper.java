c9bb9f9ea16ff8bdf8599cb3113adc260c6f800e
subah <subahmehrotra@gmail.com>
2017-08-22 23:35:40 -0700

    Need to fix neighbours and win situation

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 import java.io.*;
 import java.util.Random;
-//import static java.util.Arrays.*;
 
 public class Minesweeper{
 	
-	//Global Variables
+	// Global Variables
 	public static int row = 0;
 	public static int col = 0; 
 	public static int boom = 0;
-	boolean win = false;
-	boolean loose = false;
 	public static String bombs[][];
 	
-	//A function asking user to input the details of the screen
+	
+	/*// A function asking user to input the details of the screen
 	public static void userInput(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the number of rows");
@@ -22,27 +20,10 @@ public class Minesweeper{
 		col = input.nextInt();
 		System.out.println("Set the number of bombs");
 		boom = input.nextInt();
-	}
-	
-	//A function to place the bombs randomly
-	/*public static void placeBombs(){
-		String[][] bombs = new String[row][col+1];
-		int i;						          
-		for (i = 0; i < boom; i++){ 						    //Loop to keep a count of bombs placed
-            int ROW = (int)(Math.random() * ((row) - 0) + 0); //Getting a random row
-            int COL = (int)(Math.random() * (col) + 1); 	//Getting a random column
-            if(bombs[ROW][COL] == null){ 						//Checking if the cells is empty
-            	bombs[ROW][COL] = "X"; 						//Then place a bomb
-            }
-            else{
-            	i--; 											//Else look for another cell, without increasing the number of bombs placed
-            }
-        }
-        printCells(bombs);
-    }*/
+	}*/
     
     
-    
+    // A function to print the result after loosing the game
     public static void lost(String screen[][], String bombs[][]){
     	for(int i=0; i<bombs.length; i++){
     		for(int j=0; j<bombs[0].length; j++){
@@ -51,12 +32,13 @@ public class Minesweeper{
     			}
     		}
     	}
-    	System.out.println("You loose!");
+    	System.out.println("\n" + "You loose!" + "\n");
     	printCells(screen);
     	System.exit(0);
     }
 	
-	//A function to ask the user to select a cell
+	
+	// A function to ask the user to select a cell
 	public static String cellInput(){
 		Scanner in = new Scanner(System.in);
 		System.out.println("Select a cell by entering row and column");
@@ -67,13 +49,13 @@ public class Minesweeper{
 		System.out.println("Printing row: "+selectedRow);
 		String a = ""+selectedRow;
 		if((selectedRow-1) > row){
-			System.out.println("WRONG");
+			System.out.println("Invalid input, check the screen");
 			return "";
 		}
 		selectedCol = in.nextInt();
  		String b = ""+selectedCol;
  		if(selectedCol > col){
-			System.out.println("WRONG");
+			System.out.println("Invalid Input, check the screen");
 			return "";
 		}
 		String c = a + b;
@@ -82,6 +64,7 @@ public class Minesweeper{
 	}
 	
 	
+	// A function to check for mines at the input cell
 	public static String checkMine(String screen[][], String bombs[][], String cell){
 		int CELL = Integer.parseInt(cell);
 		int r = CELL/10;
@@ -89,9 +72,6 @@ public class Minesweeper{
 		boolean flag = true;
 		System.out.println("Printing cell: "+bombs[r][c]);
 		if(bombs[r][c] != null && bombs[r][c] != " "){
-			/*screen[r][c] = "X";
-			printCells(screen);
-			return "You lose!";*/
 			lost(screen, bombs);
 		}
 		for(int i=0; i<bombs.length-1; i++){
@@ -116,73 +96,75 @@ public class Minesweeper{
 		return "";
 	}
 	
+	
+	// A function to check for nearby mines
 	public static String minesNear(String bombs[][], int r, int c){
 		int mines = 0;
     	// check mines in all directions
     	if(r==0 && c!=1){
-    	mines += mineAt(bombs, r + 1, c - 1);  // SW
-    	mines += mineAt(bombs, r + 1, c);      // S
+    	mines += mineAt(bombs, r + 1, c - 1);  
+    	mines += mineAt(bombs, r + 1, c);      
     	mines += mineAt(bombs, r + 1, c + 1);
-    	mines += mineAt(bombs, r, c - 1);      // W
+    	mines += mineAt(bombs, r, c - 1);      
     	mines += mineAt(bombs, r, c + 1);
     	}
     	if(c==1 && r!=0){
-    	mines += mineAt(bombs, r - 1, c);      // N
+    	mines += mineAt(bombs, r - 1, c);   
     	mines += mineAt(bombs, r - 1, c + 1);
     	mines += mineAt(bombs, r, c + 1);
-    	mines += mineAt(bombs, r + 1, c);      // S
+    	mines += mineAt(bombs, r + 1, c);      
     	mines += mineAt(bombs, r + 1, c + 1);
     	}
     	if(r==bombs.length-1 && c!=bombs[0].length-1){
-    	mines += mineAt(bombs, r - 1, c - 1);  // NW
-    	mines += mineAt(bombs, r - 1, c);      // N
-    	mines += mineAt(bombs, r - 1, c + 1);  // NE
-    	mines += mineAt(bombs, r, c - 1);      // W
-    	mines += mineAt(bombs, r, c + 1);      // E
+    	mines += mineAt(bombs, r - 1, c - 1);  
+    	mines += mineAt(bombs, r - 1, c);      
+    	mines += mineAt(bombs, r - 1, c + 1); 
+    	mines += mineAt(bombs, r, c - 1);      
+    	mines += mineAt(bombs, r, c + 1);      
     	}
     	if(c==bombs[0].length-1 && r!=bombs.length-1){
-    	mines += mineAt(bombs, r - 1, c - 1);  // NW
-    	mines += mineAt(bombs, r - 1, c);      // N
-    	mines += mineAt(bombs, r, c - 1);      // W
-    	mines += mineAt(bombs, r + 1, c - 1);  // SW
-    	mines += mineAt(bombs, r + 1, c);      // S
+    	mines += mineAt(bombs, r - 1, c - 1);  
+    	mines += mineAt(bombs, r - 1, c);      
+    	mines += mineAt(bombs, r, c - 1);     
+    	mines += mineAt(bombs, r + 1, c - 1);  
+    	mines += mineAt(bombs, r + 1, c);      
     	}
-    	if(r!=0 && c!=0 && r!=bombs.length-1 && c!=bombs[0].length-1){
-    	mines += mineAt(bombs, r - 1, c - 1);  // NW
-    	mines += mineAt(bombs, r - 1, c);      // N
-    	mines += mineAt(bombs, r - 1, c + 1);  // NE
-    	mines += mineAt(bombs, r, c - 1);      // W
-    	mines += mineAt(bombs, r, c + 1);      // E
-    	mines += mineAt(bombs, r + 1, c - 1);  // SW
-    	mines += mineAt(bombs, r + 1, c);      // S
-    	mines += mineAt(bombs, r + 1, c + 1);  // SE
+    	if(r!=0 && c!=1 && r!=bombs.length-1 && c!=bombs[0].length-1){
+    	mines += mineAt(bombs, r - 1, c - 1); 
+    	mines += mineAt(bombs, r - 1, c);      
+    	mines += mineAt(bombs, r - 1, c + 1);  
+    	mines += mineAt(bombs, r, c - 1);     
+    	mines += mineAt(bombs, r, c + 1);
+    	mines += mineAt(bombs, r + 1, c - 1);  
+    	mines += mineAt(bombs, r + 1, c);    
+    	mines += mineAt(bombs, r + 1, c + 1);  
     	}
     	if(mines > 0) {
-		String mines2 = ""+(mines+48);
-      	// we're changing an int to a char
-      	// why?!
-      	// http://www.asciitable.com/
-      	// 48 is ASCII code for '0'
+		String mines2 = ""+(mines);
+      	// changing an int to a char
       	System.out.println("mines =  "+ mines2);
+      	//bombs[r][c] = mines2;
       	return mines2;
       	}
+      	//bombs[r][c] = " ";
       	return " ";
 	}
 	
 	
+	// A function that helps in counting the number of nearby mines
 	public static int mineAt(String bombs[][], int r, int c) {
-    // we need to check also that we're not out of array bounds as that would
-    // be an error
-	if(bombs[r][c]==null){
-    	return 0;
-    }
-    System.out.println("r= " + r + ", c= " + c);
-    return 1;
-    }
+		if(bombs[r][c]==null){
+			System.out.println("r= " + r + ", c= " + c);
+			System.out.println("0");
+    		return 0;
+    	}
+    	System.out.println("r= " + r + ", c= " + c);
+    	System.out.println("1");
+    	return 1;
+    	}
 	
 	
-	
-	//A function to print the cells 
+	// A function to print the cells 
 	public static void printCells(String screen[][]){
 		int k,l;
 		int size = screen[0].length;
@@ -200,22 +182,9 @@ public class Minesweeper{
 	}
 	
 	
-	//function to check if any nearby block has a bomb
-	/*public boolean nearbyBombs(int r, int c, int row, int col, char arr){
-		 int i, j;
-		 boolean rCount = false;
-		 if(r != 0 || r != row-1 || c != 1 || c != col){
-		 	if(arr[r][c+1]=='X' || arr[r][c-1]=='X' || arr[r+1][c]=='X' || arr[r-1][c]=='X'){
-		 		rCount = true;*/
-
-	//function return the number of nearby blocks having bombs
-	
-	
+	// Main function
 	public static void main(String[] args)throws IOException{
-		
 		//Asking for user input 
-		//userInput();
-		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the number of rows");
 		row = input.nextInt();
@@ -251,28 +220,20 @@ public class Minesweeper{
 		Random rand = new Random();
 		bombs = new String[row][col+1];
 		int ii;						          
-		for (ii = 1; ii <= boom; ii++){ 						    //Loop to keep a count of bombs placed
-            //int ROW = (int)(Math.random() * ((row) - 0) + 0); //Getting a random row
-            //int COL = (int)(Math.random() * ((col) - 0) + 0); 	//Getting a random column
+		for (ii = 1; ii <= boom; ii++){ 
             int ROW = rand.nextInt(row) + 0;
             int COL = rand.nextInt(col) + 1;
-            if(bombs[ROW][COL] == null){ 						//Checking if the cells is empty
-            	bombs[ROW][COL] = "X";						//Then place a bomb
+            if(bombs[ROW][COL] == null){ 						
+            	bombs[ROW][COL] = "X";						
             }
             else{
-            	ii--; 											//Else look for another cell, without increasing the number of bombs placed
+            	ii--; 											
             }
         }
-        
-        
+
 		printCells(screen);
 		printCells(bombs);
-		//placeBombs();
-		//String yo = cellInput();
-		//System.out.println(yo);
 		System.out.println(checkMine(screen, bombs, cellInput()));
-
-        
 		
 	}//end of main
 }//end of class
